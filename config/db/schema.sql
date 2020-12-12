464e19b802aff87d7383367130e02adcff79eb95
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-12-12 10:39:53 -0500

    created the schema

---
+++ config/db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS fortunes_db;
+CREATE DATABASE fortunes_db;
+USE fortunes_db;
+
+CREATE TABLE fortunes
+(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    random_fortune varchar(255) NOT NULL,
+);
